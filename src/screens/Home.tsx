3cbedd14a1bd25a85730856c043a680b8c6f5ed5
Carlos Eduardo <carlospintorossy07@gmail.com>
2023-01-27 16:03:02 -0400

    ✨ Input component created

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View, Image, TextInput } from 'react-native';
+
+import logo from '../assets/Logo.png';
 
 export function Home() {
 
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logo} />
+            </View>
+
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Adicione uma nova tarefa'
+                    placeholderTextColor='#808080'
+                />
+            </View>
+
 
         </View>
     )
@@ -14,5 +28,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#1A1A1A',
+    },
+    header: {
+        paddingTop: 100,
+        paddingBottom: 100,
+        backgroundColor: '#0D0D0D',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    form: {
+        paddingHorizontal: 24,
+        marginTop: -38
+    },
+    input: {
+        height: 56,
+        backgroundColor: '#262626',
+        borderRadius: 6,
+        color: '#ffff',
+        padding: 16,
+        fontSize: 16
     }
 })
