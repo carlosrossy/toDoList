9bafc9ba411d7506e27173b436cf3d70b9c095c3
Carlos Eduardo <carlospintorossy07@gmail.com>
2023-01-27 15:49:35 -0400

    ✨ home screen created

---
+++ src/screens/Home.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+
+export function Home() {
+
+    return (
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1A1A1A',
+    }
+})
